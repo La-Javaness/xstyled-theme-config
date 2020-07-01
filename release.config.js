4c94126d8b2b37e89c64fba7db489197e3b1b9a1
Steve Dodier-Lazaro <sdodierlazaro@gmail.com>
2020-07-01 15:27:06 +0200

    chore: Add GitHub action to release this project

--- release.config.js
+++ release.config.js
@@ -1,5 +1,23 @@
-const release = require('./.dotfiles/release')
-
-module.exports = Object.assign(release, {
+module.exports = {
+	branches: ['release'],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		'@semantic-release/npm',
+		[
+			'@semantic-release/git',
+			{
+				assets: ['build/*', 'package.json', 'yarn.lock'],
+				message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+		[
+			'@semantic-release/github',
+			{
+				assets: ['build/*', 'package.json', 'yarn.lock'],
+			},
+		],
+	],
 	repositoryUrl: 'git@github.com:La-Javaness/xstyled-theme-config.git',
-})
+}
